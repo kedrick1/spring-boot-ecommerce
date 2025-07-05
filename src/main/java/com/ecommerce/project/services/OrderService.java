04f1140b30929e6bdd13a21cb13ce9eb872d25e0
Kedrick Lavigne <kedricklavigne@gmail.com>
2025-07-04 23:09:20 -0400

    Created Order service/impl OrderControlller class with mapping to place an order. Modified wrong parameter in Payment Class

---
+++ src/main/java/com/ecommerce/project/services/OrderService.java
@@ -0,0 +1,12 @@
+package com.ecommerce.project.services;
+
+import com.ecommerce.project.dtos.OrderDTO;
+import jakarta.transaction.Transactional;
+
+
+
+public interface OrderService {
+
+    @Transactional
+    OrderDTO placeOrder(String emailId, Long addressId, String paymentMethod, String pgName, String pgPaymentId, String pgStatus, String pgResponseMessage);
+}
